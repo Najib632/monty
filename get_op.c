a20ac3fbdca2c93bb26109382af7466f39ec27f1
Najib Muhammad <najibm38@mail.com>
2024-03-29 15:23:28 +0100

    Add readme file

---
+++ get_op.c
@@ -0,0 +1,8 @@
+#include "monty.h"
+
+/**
+ * get_opcode - matches op codes to respective functions
+ * @opcode: operation code to match
+ *
+ * Return: operation function (SUCCESS), NULL (FAIL)
+ **/
